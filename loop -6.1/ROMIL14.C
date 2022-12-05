4b1726479274e4467bc6a66794bc69ba92028707
RomilVasoya <118708996+RomilVasoya@users.noreply.github.com>
2022-12-05 10:57:01 +0530

    while loop 6.1

---
+++ loop -6.1/ROMIL14.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+	int i=1,n;
+
+	clrscr();
+	printf("enter value of n = ");
+	scanf("%d",&n);
+	while(i<=n){
+		printf("%d ",n);
+		n--;
+
+		}
+		getch();
+}
